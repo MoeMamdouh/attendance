8519e31992c667bed4420bfd0aa36c6bf9ec1622
Mamdouh <m.mamdouh@symbyo.com>
2017-10-05 17:21:19 +0200

    header ui

--- index.js
+++ index.js
@@ -1,11 +1,64 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { DatePicker, message } from 'antd';
+// import { DatePicker, message, InputGroup, Select, Option, Icon } from 'antd';
+import { Input, Col, Select, InputNumber, DatePicker, AutoComplete, Cascader, message, Icon } from 'antd';
+import Moment from 'moment';
+import _ from 'lodash';
+
+const InputGroup = Input.Group;
+const Option = Select.Option;
+
+// const options = [{
+//   value: 'zhejiang',
+//   label: 'Zhejiang',
+//   children: [{
+//     value: 'hangzhou',
+//     label: 'Hangzhou',
+//     children: [{
+//       value: 'xihu',
+//       label: 'West Lake',
+//     }],
+//   }],
+// }, {
+//   value: 'jiangsu',
+//   label: 'Jiangsu',
+//   children: [{
+//     value: 'nanjing',
+//     label: 'Nanjing',
+//     children: [{
+//       value: 'zhonghuamen',
+//       label: 'Zhong Hua Men',
+//     }],
+//   }],
+// }];
+
+const names = [{
+  value: 'Mamdouh',
+  label: 'Mamdouh'
+}, {
+  value: 'Nabil',
+  label: 'Nabil'
+}, {
+  value: 'Azazy',
+  label: 'Azazy'
+}, {
+  value: 'Islam',
+  label: 'Islam'
+}];
+
+const departments = [{
+  value: 'zhejiang',
+  label: 'Zhejiang'
+}, {
+  value: 'jiangsu',
+  label: 'Jiangsu',
+}];
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      persons: [],
       date: '',
     };
   }
@@ -15,7 +68,8 @@ class App extends React.Component {
       .then(response => response.json())
       .then(data => {
         console.log('Data : ', data);
-        // this.setState({ persons: data });
+        data = Object.keys(data)
+        this.setState({ persons: data });
       })
   }
 
@@ -26,9 +80,18 @@ class App extends React.Component {
   }
 
   render() {
+    const { persons } = this.state;
+    console.log( 'persons ', persons )
     return (
-      <div style={{ width: 400, margin: '100px auto' }}>
-        <DatePicker onChange={value => this.handleChange(value)} />
+      <div style={{ width: '600px', margin: '100px auto' }}>
+        <Icon type="question" style={{ fontSize: 16, color: '#08c' }} />
+
+        <InputGroup compact>
+          <DatePicker onChange={value => this.handleChange(value)} />
+          <DatePicker onChange={value => this.handleChange(value)} />
+          <Cascader style={{ width: '20%' }} options={persons} placeholder="Select Name" />
+          <Cascader style={{ width: '20%' }} options={departments} placeholder="Select Name" />
+        </InputGroup>
         <div style={{ marginTop: 20 }}>Date: {this.state.date.toString()}</div>
       </div>
     );
