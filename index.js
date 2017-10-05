3b704b45a3d6ab5e36e20523406755ad04ae0a31
Mamdouh <m.mamdouh@symbyo.com>
2017-10-05 18:02:16 +0200

    add header bar inputs and request names and departments

--- index.js
+++ index.js
@@ -1,76 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import { DatePicker, message, InputGroup, Select, Option, Icon } from 'antd';
-import { Input, Col, Select, InputNumber, DatePicker, AutoComplete, Cascader, message, Icon } from 'antd';
+import { Spin, Alert, Input, Col, Select, InputNumber, DatePicker, AutoComplete, Cascader, message, Icon, Button } from 'antd';
 import Moment from 'moment';
 import _ from 'lodash';
 
 const InputGroup = Input.Group;
 const Option = Select.Option;
 
-// const options = [{
-//   value: 'zhejiang',
-//   label: 'Zhejiang',
-//   children: [{
-//     value: 'hangzhou',
-//     label: 'Hangzhou',
-//     children: [{
-//       value: 'xihu',
-//       label: 'West Lake',
-//     }],
-//   }],
-// }, {
-//   value: 'jiangsu',
-//   label: 'Jiangsu',
-//   children: [{
-//     value: 'nanjing',
-//     label: 'Nanjing',
-//     children: [{
-//       value: 'zhonghuamen',
-//       label: 'Zhong Hua Men',
-//     }],
-//   }],
-// }];
-
-const names = [{
-  value: 'Mamdouh',
-  label: 'Mamdouh'
-}, {
-  value: 'Nabil',
-  label: 'Nabil'
-}, {
-  value: 'Azazy',
-  label: 'Azazy'
-}, {
-  value: 'Islam',
-  label: 'Islam'
-}];
-
-const departments = [{
-  value: 'zhejiang',
-  label: 'Zhejiang'
-}, {
-  value: 'jiangsu',
-  label: 'Jiangsu',
-}];
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       persons: [],
+      departments: [],
       date: '',
     };
+    fetch("http://192.168.2.5:5050/name")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ persons: data });
+    })
+
+  fetch("http://192.168.2.5:5050/department")
+    .then(response => response.json())
+    .then(data => {
+      this.setState({ departments: data });
+    })
   }
 
-  componentDidMount() {
-    fetch("http://0.0.0.0:5050/records")
-      .then(response => response.json())
-      .then(data => {
-        console.log('Data : ', data);
-        data = Object.keys(data)
-        this.setState({ persons: data });
-      })
+  componentWillMount() {
+
+    // fetch("http://192.168.2.5:5050/records")
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log('Data : ', data);
+    //     data = Object.keys(data)
+    //     this.setState({ persons: data });
+    //   })
+
+    
   }
 
 
@@ -79,22 +48,44 @@ class App extends React.Component {
     this.setState({ date });
   }
 
-  render() {
-    const { persons } = this.state;
-    console.log( 'persons ', persons )
-    return (
-      <div style={{ width: '600px', margin: '100px auto' }}>
-        <Icon type="question" style={{ fontSize: 16, color: '#08c' }} />
+  handleOptionsData(array) {
+    let retunedArray = [];
+    _.forEach(array, function (element) {
+      let personObject = {};
+      personObject.value = element
+      personObject.label = element
+      retunedArray.push(personObject);
+    })
+    return retunedArray;
+  }
 
-        <InputGroup compact>
-          <DatePicker onChange={value => this.handleChange(value)} />
-          <DatePicker onChange={value => this.handleChange(value)} />
-          <Cascader style={{ width: '20%' }} options={persons} placeholder="Select Name" />
-          <Cascader style={{ width: '20%' }} options={departments} placeholder="Select Name" />
-        </InputGroup>
-        <div style={{ marginTop: 20 }}>Date: {this.state.date.toString()}</div>
-      </div>
-    );
+  render() {
+    const { persons, departments } = this.state;
+    console.log('persons ', persons, 'departments ', departments)
+    //if data exist
+    if (persons.length && departments.length) {
+      let personsArray = this.handleOptionsData(persons);
+      let departmentsArray = this.handleOptionsData(departments);
+      return (
+        <div style={{ width: "80%", margin: '100px auto' }}>
+          <Icon type="question" style={{ fontSize: 16, color: '#08c' }} />
+
+          <InputGroup compact>
+            <DatePicker onChange={value => this.handleChange(value)} />
+            <DatePicker onChange={value => this.handleChange(value)} />
+            <Cascader style={{ width: '30%' }} options={personsArray} placeholder="Select Name" />
+            <Cascader style={{ width: '30%' }} options={departmentsArray} placeholder="Select Separtment" />
+            <Button type="primary">Done</Button>
+          </InputGroup>
+          <div style={{ marginTop: 20 }}>Date: {this.state.date.toString()}</div>
+        </div>
+      );
+    } else {
+      return (
+        <Spin tip="Loading..."></Spin>
+      )
+    }
+
   }
 }
 
