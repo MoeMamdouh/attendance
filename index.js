0980ecbf3420374223c43d7bf65d709452f23206
Mamdouh <m.mamdouh@symbyo.com>
2017-10-05 16:40:33 +0200

    add date picker

--- index.js
+++ index.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import { DatePicker } from 'antd';
+import { DatePicker, message } from 'antd';
 
-function App() {
-  return (
-    <div style={{ margin: 100 }}>
-      <h1>AntDesign Demo</h1>
-      <hr /><br />
-      <DatePicker />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: '',
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://0.0.0.0:5050/records")
+      .then(response => response.json())
+      .then(data => {
+        console.log('Data : ', data);
+        // this.setState({ persons: data });
+      })
+  }
+
+
+  handleChange(date) {
+    message.info('Selected Date: ' + date.toString());
+    this.setState({ date });
+  }
+
+  render() {
+    return (
+      <div style={{ width: 400, margin: '100px auto' }}>
+        <DatePicker onChange={value => this.handleChange(value)} />
+        <div style={{ marginTop: 20 }}>Date: {this.state.date.toString()}</div>
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
